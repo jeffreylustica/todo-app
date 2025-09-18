8d7f597de6df61012492107531e97b7f7de24f97
jeff <jefflustica2@gmail.com>
2025-09-19 01:06:57 +0800

    feat: add handleClick function to update todo item completion status

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,26 @@
-const initialTodos = [
-  { text: "buy groceries", isCompleted: false },
-  { text: "jog", isCompleted: true },
-  { text: "study", isCompleted: false },
-];
+import { useState } from "react";
 
 const TodoList = () => {
+  const [todo, setTodo] = useState([
+    { id: 1, text: "buy groceries", isCompleted: false },
+    { id: 2, text: "jog", isCompleted: true },
+    { id: 3, text: "study", isCompleted: false },
+  ]);
+
+  const handleClick = (id) => {
+    setTodo((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
   return (
     <ul className="p-2">
-      {initialTodos.map((todo) => (
+      {todo.map((todo) => (
         <li
           key={todo.text}
           className="cursor-pointer p-2 flex justify-between hover:bg-neutral-100"
+          onClick={() => handleClick(todo.id)}
         >
           <span
             className={`${
